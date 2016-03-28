2226d11d41dee31381be38b1ab202a86520c87f0
Zura Guerra <guerrazuramx@gmail.com>
2016-03-28 14:17:56 -0600

    Solve Multiples of 3 and 5

---
+++ 1.exs
@@ -0,0 +1,24 @@
+# MULTIPLES OF 3 AND 5
+# If we list all the natural numbers below 10 
+# that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+defmodule Multiples do
+  def sum(top, dividends) do
+    coincidences = Enum.map(dividends, fn(dividend) -> 
+                     Enum.map(1..(top - 1), fn(x) ->
+                       case rem(x, dividend) do
+                         0 ->
+                           x
+                         _ ->
+                       end
+                     end)
+                   end)
+    coincidences
+    |> List.flatten
+    |> Enum.uniq
+    |> List.delete(nil)
+    |> Enum.sum
+  end 
+end
