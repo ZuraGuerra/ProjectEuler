c9974e6d18693de231388eee22549fb9b2de7736
Zura Guerra <guerrazuramx@gmail.com>
2016-03-28 14:26:26 -0600

    Add note on calling to Multiples of 3 and 5

--- 1.exs
+++ 1.exs
@@ -5,6 +5,7 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 defmodule Multiples do
+  # Call sum(100, [3, 5]) to solve the exercise
   def sum(top, dividends) do
     coincidences = Enum.map(dividends, fn(dividend) -> 
                      Enum.map(1..(top - 1), fn(x) ->
